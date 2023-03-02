f849c15321f19a67bdfcd6447071a27af9f59555
Rohit Yadav <2020.rohit.yadav@ves.ac.in>
2023-03-02 23:38:21 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Products from './component/Products';
+import Product from './component/Product';
+function App() {
+  return (
+    <>
+      <Navbar />
+      
+      <Routes>
+   
+        <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/products/:id' element={<Product/>} />
+
+      </Routes>
+    </>
+  );
+}
+
+export default App;
